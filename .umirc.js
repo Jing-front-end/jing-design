6b1763c86438c22c46d15675d21558167fbfa81d
vincent <chanyhot@hotmail.com>
2020-02-20 16:31:10 +0800

    更新umi-library至father

--- .umirc.js
+++ .umirc.js
@@ -1,20 +1,3 @@
 const url = require('postcss-url');
 
-export default {
-  plugins: [
-    [
-      'umi-plugin-library',
-      {
-        entry: 'src/index.ts',
-        esm: 'rollup',
-        cjs: 'rollup',
-        cssModules: false,
-        extraPostCSSPlugins: [
-          url({
-            url: 'inline',
-          }),
-        ],
-      },
-    ],
-  ],
-};
+export default {};
